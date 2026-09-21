c40b0d50e88ef5f1d5c6c227d2d55dd0acaf7443
Crizz-20 <cortizlupaca@gmail.com>
2026-09-21 00:26:38 -0500

    NUEVA HORA

--- script.js
+++ script.js
@@ -219,7 +219,7 @@
     var textAnimate = eval(Jscex.compile("async", function () {
         var together = new Date();
         together.setFullYear(2026, 1, 2);
-        together.setHours(0);
+        together.setHours(12);
         together.setMinutes(0);
         together.setSeconds(0);
         together.setMilliseconds(0);
